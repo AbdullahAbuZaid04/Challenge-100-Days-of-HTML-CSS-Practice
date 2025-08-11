6de6724ded41cf92e91f75d7927858d583292b70
Abdullah04 <abdullahabuzaid2004@gmail.com>
2025-08-11 13:59:30 +0300

    Add Project Day 066

---
+++ Project_Day066/main.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector("#menu-icon");
+const navlist = document.querySelector(".navlist");
+menu.onclick = () => {
+  navlist.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
